d95ca3311bd1d01d617dddd7b3555a69854349a6
Jacquelyne Glover <jackmidoglover@gmail.com>
2018-11-13 20:39:25 -0700

    created a few example components

---
+++ Angular/my-dream-app/src/app/servers/servers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  template: '<app-new></app-new>',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
