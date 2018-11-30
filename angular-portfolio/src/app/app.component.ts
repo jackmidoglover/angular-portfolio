67b3f4e4d507a70044f747b736e8e869b940f4df
Jacquelyne Glover <jackmidoglover@gmail.com>
2018-11-29 20:56:46 -0700

    my navigation is created and it is beautiful and idgaf if it's not a navbar

--- angular-portfolio/src/app/app.component.ts
+++ angular-portfolio/src/app/app.component.ts
@@ -8,8 +8,23 @@ var projects = require('../assets/projectdata/projects.json');
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   parallaxClasses = ["firstMoon", "crescentMoon", "newMoon"];
   projects = [...projects];
   sectionTitle = ["About Me", "Portfolio"];
+  sectionLinks = [
+    {
+      up: "Home", 
+      down: "Portfolio",
+      upRef: "#home",
+      downRef: "#portfolio"
+    },
+    {
+      up:"About Me",
+      down: "Contact Me",
+      upRef: "#aboutMe",
+      downRef: "#contactMe"
+    }
+  ];
 }
